62997f29240b92335cc5e2caf23e8c596f75e0b9
Fidel Lim <lim.fidel@gmail.com>
2022-01-21 21:55:26 +0400

    check if 2nd param is character

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,19 +6,18 @@
 /*   By: flim <flim@student.42abudhabi.ae>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 15:55:22 by flim              #+#    #+#             */
-/*   Updated: 2022/01/09 00:01:08 by flim             ###   ########.fr       */
+/*   Updated: 2022/01/21 19:12:05 by flim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s)
-	{
-		if (*s == c)
-			return ((char *)s);
+	if (!ft_isascii(c))
+		return ((char *)s);
+	while (*s && *s != c)
 		s++;
-	}
-	if (*s == c)
+	if (*s == c || !ft_isascii(c))
 		return ((char *)s);
 	return (0);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,15 +6,18 @@
 /*   By: flim <flim@student.42abudhabi.ae>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 15:57:00 by flim              #+#    #+#             */
-/*   Updated: 2022/01/08 23:59:29 by flim             ###   ########.fr       */
+/*   Updated: 2022/01/21 19:16:15 by flim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
 	char	*ptr;
 
 	ptr = 0;
+	if (!ft_isascii(c))
+		return ((char *)s);
 	while (*s)
 	{
 		if (*s == c)
