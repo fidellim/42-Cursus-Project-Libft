c50d7ef78e3e9d6526e775fbc86c77abce48ff49
Fidel Lim <lim.fidel@gmail.com>
2022-01-24 19:41:43 +0400

    convert var c to char

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,18 +6,16 @@
 /*   By: flim <flim@student.42abudhabi.ae>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 15:55:22 by flim              #+#    #+#             */
-/*   Updated: 2022/01/21 19:12:05 by flim             ###   ########.fr       */
+/*   Updated: 2022/01/24 19:40:02 by flim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
 {
-	if (!ft_isascii(c))
-		return ((char *)s);
-	while (*s && *s != c)
+	while (*s && *s != (char)c)
 		s++;
-	if (*s == c || !ft_isascii(c))
+	if (*s == (char)c)
 		return ((char *)s);
 	return (0);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: flim <flim@student.42abudhabi.ae>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 15:57:00 by flim              #+#    #+#             */
-/*   Updated: 2022/01/21 19:16:15 by flim             ###   ########.fr       */
+/*   Updated: 2022/01/24 19:39:11 by flim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -16,15 +16,13 @@ char	*ft_strrchr(const char *s, int c)
 	char	*ptr;
 
 	ptr = 0;
-	if (!ft_isascii(c))
-		return ((char *)s);
 	while (*s)
 	{
-		if (*s == c)
+		if (*s == (char)c)
 			ptr = (char *)s;
 		s++;
 	}
-	if (*s == c)
+	if (*s == (char)c)
 		ptr = (char *)s;
 	return (ptr);
 }
