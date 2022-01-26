01e2520c215d50c463b66af5de94e1543ee8d5e5
Fidel Lim <lim.fidel@gmail.com>
2022-01-26 15:28:28 +0400

    add checker for param

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: flim <flim@student.42abudhabi.ae>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 16:32:02 by flim              #+#    #+#             */
-/*   Updated: 2022/01/24 19:32:51 by flim             ###   ########.fr       */
+/*   Updated: 2022/01/26 15:20:43 by flim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -16,12 +16,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char			*ptr;
 	unsigned int	len_s;
 
-	if (!s || !len)
+	if (!s)
 		return (0);
 	len_s = (unsigned int)ft_strlen(s);
+	if (len > len_s)
+		len = len_s - start;
 	if (start >= len_s)
 		return (ft_strdup(""));
-	ptr = malloc (sizeof(char) * len + 1);
+	ptr = malloc (sizeof(char) * (len + 1));
 	if (!ptr)
 		return (0);
 	ft_memcpy(ptr, s + start, len);
